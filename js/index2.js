9df28ded9229aeb34a48c66042345c8cc49fe1b7
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2025-01-26 15:37:29 +0100

    Added the code for faq open and close

--- js/index2.js
+++ js/index2.js
@@ -13,4 +13,24 @@ const year = document.getElementById('year');
 year.textContent = new Date().getFullYear();
 
 
+function toggleAnswer(id) {
+    // Get all FAQ answers and questions
+    var answers = document.querySelectorAll('.faq-answer');
+    var questions = document.querySelectorAll('.faq-question');
+
+    // Hide all answers and remove active class from all questions
+    answers.forEach(function(answer) {
+      answer.classList.remove('open');
+    });
+    questions.forEach(function(question) {
+      question.classList.remove('open');
+    });
+
+    // Show the selected answer and add active class to the clicked question
+    var selectedAnswer = document.getElementById(id);
+    var selectedQuestion = document.querySelector(`[onclick="toggleAnswer('${id}')"]`);
+    selectedAnswer.classList.add('open');
+    selectedQuestion.classList.add('open');
+  }
+
 
